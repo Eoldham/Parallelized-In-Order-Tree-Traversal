54d1dd6df95ffd4195dd2d30cea05f8bbd62f669
Emily Oldham <54590979+Eoldham@users.noreply.github.com>
2021-11-28 20:41:44 -0500

    Updated so diff trees are made

--- recursive_traversal.cpp
+++ recursive_traversal.cpp
@@ -1,20 +1,39 @@
-#Code From: https://www.techiedelight.com/inorder-tree-traversal-iterative-recursive/ 
+//Code From: https://www.techiedelight.com/inorder-tree-traversal-iterative-recursive/
 
 #include <iostream>
+#include <stack>
 using namespace std;
- 
+
 // Data structure to store a binary tree node
 struct Node
 {
     int data;
     Node *left, *right;
- 
+
     Node(int data)
     {
         this->data = data;
         this->left = this->right = nullptr;
     }
 };
+
+//Code from: https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+Node* insert(Node* node, int data)
+{
+    /* If the tree is empty, return a new node */
+    if (node == nullptr)
+        return new Node(data);
+
+    /* Otherwise, recur down the tree */
+    if (data < node->data)
+        node->left = insert(node->left, data);
+    else if (data > node->data)
+        node->right = insert(node->right, data);
+
+    /* return the (unchanged) node pointer */
+    return node;
+}
+
  
 // Recursive function to perform inorder traversal on the tree
 void inorder(Node* root)
@@ -36,29 +55,30 @@ void inorder(Node* root)
  
 int main()
 {
-    /* Construct the following tree
-               1
-             /   \
-            /     \
-           2       3
-          /      /   \
-         /      /     \
-        4      5       6
-              / \
-             /   \
-            7     8
-    */
- 
+
     Node* root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left = new Node(4);
-    root->right->left = new Node(5);
-    root->right->right = new Node(6);
-    root->right->left->left = new Node(7);
-    root->right->left->right = new Node(8);
- 
-    inorder(root);
- 
+
+
+   //Height = 15
+    for (int x = 2; x <= 32767; x++){
+        insert(root,x);
+    }
+
+    /**
+    //Height = 10
+    for (int x = 2; x <= 1023; x++){
+        insert(root,x);
+    }
+    **/
+
+    /**
+    //Height = 5
+    for (int x = 2; x <= 31; x++){
+        insert(root,x);
+    }
+    **/
+
+    inorderIterative(root);
+
     return 0;
 }
